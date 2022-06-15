9d6f22217497d5a0aeae87674c518c4888c1ab62
Elton Moraes <eltonsimor@gmail.com>
2022-06-14 21:21:24 -0300

    Projeto inicial

---
+++ src/main/java/br/com/poc/SpringCloudStreamMultipleRabbitmqPocApplication.java
@@ -0,0 +1,13 @@
+package br.com.poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudStreamMultipleRabbitmqPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudStreamMultipleRabbitmqPocApplication.class, args);
+	}
+
+}
